7e41732ce091dbb7480ff272c35997816e6bc097
bharatak <bharatak@thoughtworks.com>
2017-12-08 13:17:54 +0530

    Support for volumes in AWS and GCP

--- dss-docker/run.sh
+++ dss-docker/run.sh
@@ -3,10 +3,13 @@
 DSS_INSTALLDIR="/home/dataiku/dataiku-dss-$DSS_VERSION"
 
 if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
+	echo "Changing the owner of $DSS_DATADIR"
+	chown dataiku:dataiku "$DSS_DATADIR"
+
 	# Initialize new data directory
-	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
-	"$DSS_DATADIR"/bin/dssadmin install-R-integration
-	echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties
+	su dataiku -c "$DSS_INSTALLDIR/installer.sh -d $DSS_DATADIR -p $DSS_PORT"
+	su dataiku -c "$DSS_DATADIR/bin/dssadmin install-R-integration"
+	su dataiku -c 'echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties'
 
 elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLDIR"') != "$DSS_INSTALLDIR" ]; then
 	# Upgrade existing data directory
@@ -15,4 +18,4 @@ elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLD
 
 fi
 
-exec "$DSS_DATADIR"/bin/dss run
+su dataiku -c 'exec "$DSS_DATADIR"/bin/dss run'
