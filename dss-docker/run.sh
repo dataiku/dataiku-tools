62b770bd737b67327e82acc4dcaf032decb2050f
Patrice Bertin <patrice2bertin@gmail.com>
2022-02-17 12:29:12 +0100

    Community edition Docker image: rebase on AlmaLinux 8
    Also preinstall graphics-export support

--- dss-docker/run.sh
+++ dss-docker/run.sh
@@ -5,13 +5,16 @@ DSS_INSTALLDIR="/home/dataiku/dataiku-dss-$DSS_VERSION"
 if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
 	# Initialize new data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
-	"$DSS_DATADIR"/bin/dssadmin install-R-integration
+	"$DSS_DATADIR"/bin/dssadmin install-R-integration -noDeps
+	"$DSS_DATADIR"/bin/dssadmin install-graphics-export
 	echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties
+	echo "dku.exports.chrome.sandbox=false" >>"$DSS_DATADIR"/config/dip.properties
 
 elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLDIR"') != "$DSS_INSTALLDIR" ]; then
 	# Upgrade existing data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -u -y
-	"$DSS_DATADIR"/bin/dssadmin install-R-integration
+	"$DSS_DATADIR"/bin/dssadmin install-R-integration -noDeps
+	"$DSS_DATADIR"/bin/dssadmin install-graphics-export
 
 fi
 
